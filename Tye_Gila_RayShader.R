014df3b91d59329de40a73c4a84e4325e1b48073
simontye <stye@icloud.com>
2020-01-12 20:52:16 -0600

    20100112 SPT
    Added 3D map w/ terrain and shapefiles

--- Tye_Gila_RayShader.R
+++ Tye_Gila_RayShader.R
@@ -33,9 +33,59 @@ setwd("/Users/simontye/Documents/Research/Projects/Gila_River/2020_Gila")
 ###############################################################
 ### Functions
 
+# Download USGS elevation data
+get_usgs_elevation_data <- function(bbox, size = "400,400", file = NULL, 
+                                    sr_bbox = 4326, sr_image = 4326) {
+  require(httr)
+  
+  # TODO - validate inputs
+  
+  url <- parse_url("https://elevation.nationalmap.gov/arcgis/rest/services/3DEPElevation/ImageServer/exportImage")
+  res <- GET(
+    url, 
+    query = list(
+      bbox = paste(bbox$p1$long, bbox$p1$lat, bbox$p2$long, bbox$p2$lat,
+                   sep = ","),
+      bboxSR = sr_bbox,
+      imageSR = sr_image,
+      size = size,
+      format = "tiff",
+      pixelType = "F32",
+      noDataInterpretation = "esriNoDataMatchAny",
+      interpolation = "+RSP_BilinearInterpolation",
+      f = "json"
+    )
+  )
+  
+  if (status_code(res) == 200) {
+    body <- content(res, type = "application/json")
+    # TODO - check that bbox values are correct
+    # message(jsonlite::toJSON(body, auto_unbox = TRUE, pretty = TRUE))
+    
+    img_res <- GET(body$href)
+    img_bin <- content(img_res, "raw")
+    if (is.null(file)) 
+      file <- tempfile("elev_matrix", fileext = ".tif")
+    writeBin(img_bin, file)
+    message(paste("image saved to file:", file))
+  } else {
+    warning(res)
+  }
+  invisible(file)
+}
+ # Read elevation data
+read_elevation_file <- function(file) {
+  elev_img <- raster::raster(file)
+  elev_matrix <- matrix(
+    raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
+    nrow = ncol(elev_img), ncol = nrow(elev_img)
+  )
+  elev_matrix
+}
+
 # Downloading a map from ArcGIS API
 get_arcgis_map_image <- function(bbox, map_type = "World_Imagery", file = NULL, 
-                                 width = 1000, height = 1000, sr_bbox = 4326) {
+                                 width = 400, height = 400, sr_bbox = 4326) {
   require(httr)
   require(glue) 
   require(jsonlite)
@@ -92,7 +142,7 @@ get_arcgis_map_image <- function(bbox, map_type = "World_Imagery", file = NULL,
 ###############################################################
 
 # Define image size variables from bounding box coordinates
-define_image_size <- function(bbox, major_dim = 1000) {
+define_image_size <- function(bbox, major_dim = 400) {
   # calculate aspect ration (width/height) from lat/long bounding box
   aspect_ratio <- abs((bbox$p1$long - bbox$p2$long) / (bbox$p1$lat - bbox$p2$lat))
   # define dimensions
@@ -201,26 +251,18 @@ unlink(loadzip)
 # Convert geotiff to raster
 Gila.raster <- raster("Gila_2016.tif")
 
-# Create sample 2D plot of raster file
-#plot(Gila, col = rev(terrain.colors(50)))
-
 # Resample raster file to smaller resolution
 Gila.raster2 <- aggregate(Gila.raster, fact = 50)
-####Gila.raster2 <- aggregate(Gila.raster, fact = 10) Try this?
 
 # Convert raster to matrix for further plotting
 Gila.map = raster_to_matrix(Gila.raster2)
 
-# Create sample 3D plot of matrix file
+# Create sample 2D plot
 Gila.map %>%
   sphere_shade(texture = "desert") %>%
   add_shadow(ray_shade(Gila.map)) %>%
   add_shadow(ambient_shade(Gila.map)) %>%
   plot_map()
-
-#%>%
-#  plot_3d(Gila.map, zscale = 5, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
-
 #Sys.sleep(0.2)
 #render_snapshot(clear=TRUE)
 
@@ -228,13 +270,13 @@ Gila.map %>%
 ### Rayshader - Version 2
 ###############################################################
 
-# Define the bounding box
+# Define bounding box
 bbox <- list(
   p1 = list(long = -108.631, lat = 32.898), # bottom left
   p2 = list(long = -108.469, lat = 33.077) # top right
 )
 
-# Display the bounding box
+# Display bounding box
 leaflet() %>%
   addTiles() %>% 
   addRectangles(
@@ -246,249 +288,156 @@ leaflet() %>%
     lng1 = bbox$p1$long, lat1 = bbox$p1$lat,
     lng2 = bbox$p2$long, lat2 = bbox$p2$lat,
   )
-# Define image size for map image files
-image_size <- define_image_size(bbox, major_dim = 1000)
+# Define image size
+image_size <- define_image_size(bbox, major_dim = 400)
 
-# Specify projection
-prj <- "+proj=utm +zone=12 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+# Download elevation data
+elev_file <- file.path("sf-elevation.tif")
+get_usgs_elevation_data(bbox, size = image_size$size, file = elev_file,
+                        sr_bbox = 4326, sr_image = 4326)
 
-# Elevation zoom
-#elevation_raster_zoom <- 10
-
-# Create bounding box wtih buffer
-bbox <- st_bbox(Gila)
-bbox["xmin"] <- bbox$xmin - 0.2
-bbox["xmax"] <- bbox$xmax + 0.2
-bbox["ymin"] <- bbox$ymin - 0.2
-bbox["ymax"] <- bbox$ymax + 0.2
-
-# View bounding box with buffer
-mapview(bbox)
-
-# Find elevation raster around path  ----
-#https://stackoverflow.com/questions/54165356/create-topographic-map-in-r
-
-# Generate a dataframe of lat/long coordinates for get_elev_raster()
-ex.df <- data.frame(x=seq(from = bbox$xmin , to = bbox$xmax, length.out = 1000), 
-                    y=seq(from = bbox$ymin , to = bbox$ymax, length.out = 1000))
-
-# Use elevatr package to get elevation data for each point.
-elev <- get_elev_raster(ex.df, prj = prj, z = 5, clip = "bbox")
-
-# Plot 2D topographical map
-raster::contour(elev)
-
-# Read shapefile
-Gila.shape <- st_read("Gila_Rivers.shp")
-
-# Find elevation for path from elevation raster
-latlons <- map_df(Gila.shape$geometry, ~ st_coordinates(.x) %>% as_tibble() %>% rename(x = X, y = Y ) ) %>% as.data.frame
-
-# ???
-z <- raster::extract(elev, latlons %>% .[, c("x","y")])
-
-# ???
-Gila.Channels  <- latlons %>% add_column(z = z) 
-Gila.Channels  <- subset(Gila.Channels, select = c(x, y, z))
-
-Gila.Channels %>%
-  select = c(x, y, z) %>%
-  as_tibble() %>%
-  mutate(is_max =  ifelse(z == max(z), 1, 0), # chop return
-         cumsum_is_max = cumsum(is_max)) %>%
-  filter(cumsum_is_max == 0 | (is_max == 1 & cumsum_is_max == 1)) %>%
-  select(y, x, z)  %>%
-  mutate(z = z + 30)
-
-# Create second bounding box to overlay image of terrain
-bbox2 <- list(
-  p1 = list(long = bbox$xmin, lat = bbox$ymin),
-  p2 =  list(long = bbox$xmax, lat = bbox$ymax)
+# Load elevation data
+elev_img <- raster(elev_file)
+elev_matrix <- matrix(
+  raster::extract(elev_img, raster::extent(elev_img), buffer = 1000), 
+  nrow = ncol(elev_img), ncol = nrow(elev_img)
 )
 
-# Download overlay image of terrain
-get_arcgis_map_image(bbox2, map_type = "World_Imagery", file = overlay_file,
-                     width = dim(elev)[2], height = dim(elev)[1], 
+# Calculate rayshader layers
+ambmat <- ambient_shade(elev_matrix, zscale = 30)
+raymat <- ray_shade(elev_matrix, zscale = 30, lambert = TRUE)
+
+# Overlay map image for the terrain
+overlay_file <- "terrain.png"
+get_arcgis_map_image(bbox, map_type = "World_Imagery", file = overlay_file,
+                     width = image_size$width, height = image_size$height, 
                      sr_bbox = 4326)
-
-# Import overlay image of terrain
-overlay_file <- "images.png"
-
-# Use when overlay image doesn't need to be rotated
 overlay_img <- png::readPNG(overlay_file)
 
-# Use when overlay image needs to be roated
-#image_write(image_rotate(image_read(overlay_file), 90) ,"rotated.png")
-#overlay_img <- png::readPNG("rotated.png")
+# 2D plot with map overlay
+#elev_matrix %>%
+#  sphere_shade(texture = "imhof4") %>%
+#  add_water(watermap, color = "imhof4") %>%
+#  add_shadow(raymat, max_darken = 0.5) %>%
+#  add_shadow(ambmat, max_darken = 0.5) %>%
+#  add_overlay(overlay_img, alphalayer = 0.5) %>%
+#  plot_map()
 
-# reconfigure bathymetry data for 3D plotting (** to correct mirrored plotting in rayshader)
-#Gila.map <- as.matrix(elev) %>%
-  #apply(., 2, rev)
-
-#myzscale = 20000 / (elevation_raster_zoom * elevation_raster_zoom * elevation_raster_zoom)
-#shadow = ray_shade(Gila.map,zscale=myzscale,lambert=FALSE)
-#ambient = ambient_shade(Gila.map,zscale=myzscale)
-
-# plot_3d
-# sphere_shade retourne RGB array of hillshaded texture mappings.
-Gila.map %>%
-  sphere_shade(texture = "desert") %>% 
-  add_shadow(ray_shade(Gila.map)) %>%
-  add_shadow(ambient_shade(Gila.map)) %>%
-  plot_map(Gila.map, zscale = 5 ,fov = 0, theta = 80, phi = 45, windowsize = c(1000, 1000), zoom = 1)
-
-#### RIVERS
+###############################################################
 
 # Read shapefile
 Gila.shape <- st_read("Gila_Rivers.shp")
+Gila.shape2 <-raster(Gila.shape)
 
-#elev = raster("./rayshader_issue/elev.tif")
+# Convert NHD to SpatialLines
 river = rgdal::readOGR("Gila_Rivers.shp")
-
-#Convert NHD to SpatialLines
 river = as(river, "SpatialLines")
 
-# Create Raster with equal extent, dim and res to elev with all values = 0 
+# Reproject coordinate system
+river <- spTransform(river, CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
+
+# Create empty raster
 r = raster()
-extent(r) = extent(Gila.raster)
-dim(r) = dim(Gila.raster)
-res(r) = res(Gila.raster)
-crs(r) = crs(Gila.raster)
+extent(r) = extent(elev_img)
+dim(r)    = dim(elev_img)
+res(r)    = res(elev_img)
+crs(r)    = crs(elev_img)
 values(r) = 0
 
-# Resample raster file to smaller resolution
+# Resample raster file to lower resolution
 #river2 <- aggregate(river, fact = 10)
+#r2 <- aggregate(r, fact = 10)
 
-# Rasterize rivers to new raster;  set water to 1 and all else to 0
+# Combine river with empty raster; water = 1, terrain = 0
 rr = rasterize(river, r)
 values(rr)[is.na(values(rr))] = 0
 values(rr)[values(rr) != 0] = 1
 
-#rr
-#elev
+# Create water matrix
+water = matrix(raster::extract(rr,raster::extent(rr),buffer = 1000), nrow = ncol(rr),ncol = nrow(rr))
 
-water = matrix(raster::extract(rr,raster::extent(rr),
-               nrow=ncol(rr),ncol=nrow(rr)))
+# Horizontally flip terrain image
+water = apply(water, 2, rev)
 
-water =   matrix(values(rr),
-                 nrow=ncol(rr),ncol=nrow(rr))
+# Check dimensions
+dim(water) == dim(elev_matrix)
 
-#water[nrow(water):1,] or water[,ncol(water):1]
-water1 = apply(water, 2, rev)
-water2 = apply(t(water1),2,rev)
-water2 = apply(t(water2),2,rev)
+# 2D plot with terrain and river
+elev_matrix %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(water, color = "lightblue") %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  plot_map()
 
-
-elevmat = matrix(raster::extract(elev,raster::extent(elev),buffer=10000),
-                 nrow=ncol(elev),ncol=nrow(elev))
-
-##check dims
-#dim(water) == dim(elevmat)
-
-# Rayshade!!
-Gila.map %>%
+# 3D plot with terrain and river
+zscale <- 10
+rgl::clear3d()
+elev_matrix %>% 
   sphere_shade(texture = "desert") %>% 
-  add_water(water1, color="lightblue") %>% 
-  plot_map
+  add_water(water, color = "lightblue") %>%
+  add_overlay(overlay_img, alphalayer = 0.5) %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  plot_3d(elev_matrix, zscale = zscale, windowsize = c(1200, 1000),
+          water = TRUE, soliddepth = -max(elev_matrix)/zscale, wateralpha = 0,
+          theta = 25, phi = 30, zoom = 0.65, fov = 60)
+render_snapshot()
+
+###############################################################
+### GIF
+
+# montery water gif ====
+elev_matrix <- montereybay
+n_frames <- 180
+zscale <- 50
+# frame transition variables
+waterdepthvalues <- min(elev_matrix)/2 - min(elev_matrix)/2 * cos(seq(0,2*pi,length.out = n_frames))
+thetavalues <- -90 + 45 * cos(seq(0, 2*pi, length.out = n_frames))
+# shadow layers
+ambmat <- ambient_shade(elev_matrix, zscale = zscale)
+raymat <- ray_shade(elev_matrix, zscale = zscale, lambert = TRUE)
+
+# generate .png frame images
+img_frames <- paste0("drain", seq_len(n_frames), ".png")
+for (i in seq_len(frames)) {
+  message(paste(" - image", i, "of", n_frames))
+  elev_matrix %>%
+    sphere_shade(texture = "imhof1") %>%
+    add_shadow(ambmat, 0.5) %>%
+    add_shadow(raymat, 0.5) %>%
+    plot_3d(elev_matrix, solid = TRUE, shadow = TRUE, zscale = zscale, 
+            water = TRUE, watercolor = "imhof3", wateralpha = 0.8, 
+            waterlinecolor = "#ffffff", waterlinealpha = 0.5,
+            waterdepth = waterdepthvalues[i]/zscale, 
+            theta = thetavalues[i], phi = 45)
+  render_snapshot(img_frames[i])
+  rgl::clear3d()
+}
+
+# build gif
+magick::image_write_gif(magick::image_read(img_frames), 
+                        path = "montereybay.gif", 
+                        delay = 6/n_frames)
+
+# calculate input vectors for gif frames
+n_frames <- 180
+waterdepths <- transition_values(from = 0, to = min(montereybay), steps = n_frames) 
+thetas <- transition_values(from = -45, to = -135, steps = n_frames)
+# generate gif
+zscale <- 50
+montereybay %>% 
+  sphere_shade(texture = "imhof1", zscale = zscale) %>%
+  add_shadow(ambient_shade(montereybay, zscale = zscale), 0.5) %>%
+  add_shadow(ray_shade(montereybay, zscale = zscale, lambert = TRUE), 0.5) %>%
+  save_3d_gif(montereybay, file = "montereybay.gif", duration = 6,
+              solid = TRUE, shadow = TRUE, water = TRUE, zscale = zscale,
+              watercolor = "imhof3", wateralpha = 0.8, 
+              waterlinecolor = "#ffffff", waterlinealpha = 0.5,
+              waterdepth = waterdepths/zscale, 
+              theta = thetas, phi = 45)
 
 
-# Create first 3D plot
-#Gila.map %>%
-#  sphere_shade(texture = "desert") %>%
-#  add_shadow(ray_shade(Gila.map)) %>%
-#  add_shadow(ambient_shade(Gila.map)) %>%
-#  plot_3d(Gila.map, zscale = 5, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
-#
-#
-#
-##check dims
-#dim(water) == dim(elevmat)
-#
-#
-##KUD3D adds points and axes
-#add_points(
-#  ras = elev,
-#  det = Gila.Channels,
-#  zscale=myzscale,
-#  cont = c(95, 5),
-#  alphavec = c(0.1, 0.9),
-#  drawpoints = T,
-#  size = 3,
-#  col.pt = "black",
-#  colors = c("red","red")
-#)  
-#add_axes(elev,
-#         zscale=myzscale,
-#         axis.col = grey(0.5))
-#
-#render_snapshot("trailmap")
 
 
-#### generate gif
-## elev_matrix <- elmat
-# n_frames <- 60
-# zscale <- myzscale
-# # frame transition variables
-# thetavalues <- transition_values(from = 80, to = 35, steps = n_frames, 
-#                                  one_way = TRUE, type = "lin")
-# 
-# phivalues <- transition_values(from = 28, to = 73, steps = n_frames, 
-#                                one_way = FALSE, type = "cos")
-# 
-# zoomvalues <- transition_values(from = 0.6, to = 0.8, steps = n_frames, 
-#                                 one_way = FALSE, type = "cos")
-# # shadow layers
-# ambmat <- ambient_shade(elev_matrix, zscale = zscale)
-# raymat <- ray_shade(elev_matrix, zscale = zscale, lambert = TRUE)
-# 
-# # generate .png frame images
-# img_frames <- paste0("drain", seq_len(n_frames), ".png")
-# for (i in seq_len(n_frames)) {
-#   message(paste(" - image", i, "of", n_frames))
-#   
-#   sphere_shade(heightmap = elmat, zscale=myzscale,texture = "imhof4") %>% 
-#     add_shadow(shadow) %>%
-#     add_shadow(ambient)  %>% 
-#     #add_water(detect_water(elmat)) %>%
-#     add_overlay(., overlay = overlay_img, alphacolor = NULL,alphalayer = 0.9)  %>%
-#     plot_3d(., heightmap = elmat,
-#             zscale=myzscale,fov=0,theta=thetavalues[i],phi=phivalues[i],windowsize=c(1000,800),zoom=zoomvalues[i],
-#             water=FALSE,          baseshape = "circle" 
-#     )
-#   
-#   
-#   # render labels
-#   # locaton founds by trial and error
-#   render_label(heightmap = elmat,x=363,y=281, z=1000,zscale=myzscale,
-#                text = "Base Camp",textsize = 2,linewidth = 5)
-#   
-#   
-#   render_label(heightmap = elmat,x=124,y=178, z=1000,zscale=myzscale,
-#                text = "Summit",textsize = 2,linewidth = 5)
-#   
-#   
-#   #KUD3D adds points and axes
-#   add_points(
-#     ras = elev,
-#     det = kilian_path,
-#     zscale=myzscale,
-#     cont = c(95, 5),
-#     alphavec = c(0.1, 0.9),
-#     drawpoints = T,
-#     size = 3,
-#     col.pt = "black",
-#     colors = c("red","red")
-#   )  
-#   add_axes(elev,
-#            zscale=myzscale,
-#            axis.col = grey(0.5))
-#   
-#   
-#   render_snapshot(img_frames[i])
-#   rgl::clear3d()
-# }
-# 
-# # build gif
-# magick::image_write_gif(magick::image_read(img_frames), 
-#                         path = "everest.gif", 
-#                         delay = 2/n_frames)
